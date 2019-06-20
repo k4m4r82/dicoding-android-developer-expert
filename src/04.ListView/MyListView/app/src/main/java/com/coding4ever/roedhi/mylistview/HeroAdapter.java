ec7c32dcd10cffc7aed8dce3a535759d28c72c8f
kamarudin <kamarudin@amikom.ac.id>
2019-06-20 15:15:59 +0700

    add ListView project

---
+++ src/04.ListView/MyListView/app/src/main/java/com/coding4ever/roedhi/mylistview/HeroAdapter.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (C) 2019 Kamarudin (http://coding4ever.net/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * The latest version of this file can be found at https://github.com/k4m4r82/dicoding-android-developer-expert
+ */
+
+package com.coding4ever.roedhi.mylistview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class HeroAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Hero> heroes;
+
+    public void setHeroes(ArrayList<Hero> heroes) {
+        this.heroes = heroes;
+    }
+
+    public HeroAdapter(Context context) {
+        this.context = context;
+        heroes = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount() {
+        return heroes.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return heroes.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.item_hero, viewGroup, false);
+        }
+
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        Hero hero = (Hero) getItem(i);
+        viewHolder.bind(hero);
+
+        return view;
+    }
+
+    private class ViewHolder {
+        private TextView txtName;
+        private TextView txtDescription;
+        private ImageView imgPhoto;
+
+        ViewHolder(View view) {
+            txtName = view.findViewById(R.id.txt_name);
+            txtDescription = view.findViewById(R.id.txt_description);
+            imgPhoto = view.findViewById(R.id.img_photo);
+        }
+
+        void bind(Hero hero) {
+            txtName.setText(hero.getName());
+            txtDescription.setText(hero.getDescription());
+            imgPhoto.setImageResource(hero.getPhoto());
+        }
+    }
+}
---
+++ src/04.ListView/MyListView/app/src/main/java/com/coding4ever/roedhi/mylistview/MainActivity.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (C) 2019 Kamarudin (http://coding4ever.net/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * The latest version of this file can be found at https://github.com/k4m4r82/dicoding-android-developer-expert
+ */
+
+package com.coding4ever.roedhi.mylistview;
+
+import android.content.res.TypedArray;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // private String[] dataName = {"Cut Nyak Dien","Ki Hajar Dewantara","Moh Yamin","Patimura","R A Kartini","Sukarno"};
+
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+    private HeroAdapter adapter;
+    private ArrayList<Hero> heroes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.lv_list);
+        /*ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, dataName);*/
+
+        adapter = new HeroAdapter(this);
+        listView.setAdapter(adapter);
+
+        prepare();
+        addItem();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, heroes.get(i).getName(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+    }
+
+    private void addItem() {
+        heroes = new ArrayList<>();
+
+        for (int i = 0; i < dataName.length; i++) {
+            Hero hero = new Hero();
+            hero.setPhoto(dataPhoto.getResourceId(i, -1));
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+
+            heroes.add(hero);
+        }
+
+        adapter.setHeroes(heroes);
+    }
+}
