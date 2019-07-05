593a5ef5222a9400a60bc41550f0e4268fbf43b7
kamarudin <kamarudin@amikom.ac.id>
2019-07-05 11:02:14 +0700

    Tambah input pencarian di actionbar

--- src/11.Navigation/MyActionBar/app/src/main/java/net/coding4ever/roedhi/myactionbar/MainActivity.java
+++ src/11.Navigation/MyActionBar/app/src/main/java/net/coding4ever/roedhi/myactionbar/MainActivity.java
@@ -18,14 +18,18 @@
  
 package net.coding4ever.roedhi.myactionbar;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +43,25 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        if (searchManager != null) {
+            SearchView searchView = (SearchView) (menu.findItem(R.id.search)).getActionView();
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setQueryHint(getResources().getString(R.string.search_hint));
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            });
+        }
 
         return true;
     }
@@ -59,6 +82,8 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(this,MenuActivity.class);
                 startActivity(i);
                 return true;
+
+
             default:
                 return true;
         }
